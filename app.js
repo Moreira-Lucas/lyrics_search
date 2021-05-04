e533e890023ba58e0dff27cd7b9e720f1ffcc3d7
lucg23 <lucasferreira.23m@gmail.com>
2021-05-04 20:24:06 -0300

    fix bug with cors-any replace by secret-ocean

--- app.js
+++ app.js
@@ -3,8 +3,39 @@ const searchInput = document.querySelector('#search');
 const songsContainer = document.querySelector('#songs-container');
 const prevAndNextContainer = document.querySelector('#prev-and-next-container');
 
+const apiURL = `https://api.lyrics.ovh`;
+
+const getMoreSongs = async (url) =>{
+  const response = await fetch(`https://secret-ocean-49799.herokuapp.com/${url}`);
+  const data = await response.json();
+
+  insertSongsIntoPages(data);
+}
+
 const insertSongsIntoPages = songsInfo =>{
-  songsContainer.innerHTML = songsInfo.data.map();
+  songsContainer.innerHTML = songsInfo.data.map(song => `
+  <li class="song">
+    <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+    <button 
+      class="btn" data-artist="${song.artist.name}" 
+      data-song-title="${song.title}">
+      Ver Letra
+    </button>
+  
+  </li>`).join('');
+
+    
+    
+  if(songsInfo.prev || songsInfo.next){
+    prevAndNextContainer.innerHTML =`
+    ${songsInfo.prev ? `<button class="btn" onclick="getMoreSongs('${songsInfo.prev}')">Anterior</button>` :''}
+    ${songsInfo.next ? `<button class="btn" onclick="getMoreSongs('${songsInfo.next}')">Próxima</button>` :''}
+    `
+
+    return
+  }
+
+  prevAndNextContainer.innerHTML="";
 }
 
 
@@ -34,4 +65,4 @@ form.addEventListener('submit',e=>{
 })
 
 
-//const apiURL = `https://api.lyrics.ovh`;
+
